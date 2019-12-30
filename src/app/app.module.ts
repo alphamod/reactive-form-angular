7ba1af7ff20c76b6da6bee0add1f3993f47aa2bb
Syed Salman <37833818+alphamod@users.noreply.github.com>
2019-12-31 00:54:39 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainFormComponent } from './main-form/main-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainFormComponent
   ],
   imports: [
     BrowserModule,
